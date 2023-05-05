1cc78aa8b7fefca05488ec0d3e247454931b0df9
Vibhor Bhatt <108974586+vibhorbhatt777@users.noreply.github.com>
2023-05-05 11:05:25 +0530

    Add files via upload

---
+++ permutation.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+#define ll          long long
+#define p7          1000000007
+#define p9          998244353
+#define pb          push_back
+#define ff          first
+#define ss          second
+#define vll         vector<ll>
+#define pll         pair<ll,ll>
+#define vpll        vector<pll>
+#define vvll        vector<vll>
+#define mll         map<ll,ll>
+#define test        ll T;cin>>T;while(T--)
+#define ad(a,n)     vll a;a.resize(n);for(ll i=0;i<n;i++)cin>>a[i];
+#define as(a,n)     for(ll i=0;i<n;i++){cout<<a[i]<<" ";}cout<<endl;
+#define all(x)      x.begin(),x.end()
+
+//typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+ll powm(ll x, ll y, ll m = p7) {
+	x = x % m; ll res = 1; while (y)
+	{if (y & 1)res = res * x; res %= m; y = y >> 1; x = x * x; x %= m;} return res;
+}
+ll modi(ll a, ll m = p7) {return powm(a, m - 2, m);}
+void yesno(bool f) {f ? cout << "YES" << endl : cout << "NO" << endl;}
+
+
+void vb77()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+}
+
+int32_t main()
+{
+	vb77();
+	// test
+	{
+		ll n; cin >> n;
+		if (n > 1  && n < 4)
+		{
+			cout << "NO SOLUTION" << endl;
+			return 0;
+		}
+		for (ll i = 2; i <= n; i += 2) cout << i << " ";
+		for (ll i = 1; i <= n; i += 2) cout << i << " ";
+	}
+	return 0;
+}
